4c25f2a45b419e26f0e94ae33ccd050ef30c8452
naohiro <nao114computer@gmail.com>
2021-12-31 21:24:08 +0900

    test

--- index.js
+++ index.js
@@ -10,17 +10,21 @@ client.on('voiceStateUpdate', async (oldState, newState) => await onVoiceStateUp
 async function onVoiceStateUpdate(oldState, newState) {
     if (newState && oldState) {
         if (oldState.channelId === null && newState.channelId != null) {
-            newState.member.roles.cache.map(function (role) {
-                if (role.name == "VC Entry") {
-                    const Embed = new MessageEmbed()
-                        .setColor(newState.member.displayColor)
-                        .setTitle(newState.member.displayName + "が" + newState.channel.name + "に入室しました！")
-                        .setAuthor("VC入室", newState.member.displayAvatarURL())
-                        .setDescription("現在の参加者数は" + String(newState.channel.members.size) + "人です。")
-                        .setFooter('Version2.0')
-                    newState.guild.systemChannel.send({ embeds: [Embed] }).catch(console.error);
-                }
+            newState.member.map((member) => {
+                const role = member.roles.cache
+                console.log(role.name)
             });
+            // .roles.cache.map(function (role) {
+            //     if (role.name == "VC Entry") {
+            //         const Embed = new MessageEmbed()
+            //             .setColor(newState.member.displayColor)
+            //             .setTitle(newState.member.displayName + "が" + newState.channel.name + "に入室しました！")
+            //             .setAuthor("VC入室", newState.member.displayAvatarURL())
+            //             .setDescription("現在の参加者数は" + String(newState.channel.members.size) + "人です。")
+            //             .setFooter('Version2.0')
+            //         newState.guild.systemChannel.send({ embeds: [Embed] }).catch(console.error);
+            //     }
+            // });
         }
 
         if (oldState.channelId != null && newState.channelId === null) {
