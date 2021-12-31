08233ffe7db151256d0295b887e9266c17957790
nobu <53887391+nobu114@users.noreply.github.com>
2021-12-31 22:25:00 +0900

    コードの全体的な見直し (#3)

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const client = new Client({ intents: Object.keys(Intents.FLAGS) })
 
 const version = '2.1';
 
+const send_vc_notify = (voicestate, title) => {
+    const displayColor = voicestate.member.displayColor;
+    const displayName = voicestate.member.displayName;
+    const ChannelName = voicestate.channel.name;
+    const displayAvatarURL = voicestate.member.displayAvatarURL();
+    const ChannelMemberSize = voicestate.channel.members.size;
+    const Embed = new MessageEmbed()
+        .setColor(displayColor)
+        .setTitle(displayName + "が" + ChannelName + "に入室しました！")
+        .setAuthor(title, displayAvatarURL)
+        .setDescription("現在の参加者数は" + String(ChannelMemberSize) + "人です。")
+        .setFooter('Version' + version)
+    voicestate.guild.systemChannel.send({ embeds: [Embed] }).catch(console.error);
+}
+
 client.once('ready', () => {
     console.log('ログインしました。');
 })
 
-client.on('voiceStateUpdate', async (oldState, newState) => await onVoiceStateUpdate(oldState, newState))
-
-async function onVoiceStateUpdate(oldState, newState) {
+client.on('voiceStateUpdate', (oldState, newState) => {
     if (newState && oldState) {
         if (oldState.channelId === null && newState.channelId != null) {
             const roles = newState.member.roles.cache;
@@ -17,13 +30,7 @@ async function onVoiceStateUpdate(oldState, newState) {
             if (!is_bot) {
                 roles.map((role) => {
                     if (role.name == "VC Entry") {
-                        const Embed = new MessageEmbed()
-                            .setColor(newState.member.displayColor)
-                            .setTitle(newState.member.displayName + "が" + newState.channel.name + "に入室しました！")
-                            .setAuthor("VC入室", newState.member.displayAvatarURL())
-                            .setDescription("現在の参加者数は" + String(newState.channel.members.size) + "人です。")
-                            .setFooter('Version' + version)
-                        newState.guild.systemChannel.send({ embeds: [Embed] }).catch(console.error);
+                        send_vc_notify(newState, "VC入室");
                     }
                 });
             }
@@ -32,21 +39,14 @@ async function onVoiceStateUpdate(oldState, newState) {
             const roles = oldState.member.roles.cache;
             const is_bot = oldState.member.user.bot;
             if (!is_bot) {
-                roles.map((role) =>{
-                    if (role.name == "VC Exit"){
-                        const Embed = new MessageEmbed()
-                        .setColor(oldState.member.displayColor)
-                        .setTitle(oldState.member.displayName + "が" + oldState.channel.name + "から退室しました！")
-                        .setAuthor("VC退室", oldState.member.displayAvatarURL())
-                        .setDescription("現在の参加者数は" + String(oldState.channel.members.size) + "人です。")
-                        .setFooter('Version' + version)
-                        oldState.guild.systemChannel.send({ embeds: [Embed] }).catch(console.error);
+                roles.map((role) => {
+                    if (role.name == "VC Exit") {
+                        send_vc_notify(oldState, "VC退室");
                     }
                 });
             }
         }
     }
+})
 
-};
-
-client.login(process.env.DISCORD_TOKEN)
+client.login(process.env.DISCORD_TOKEN);
